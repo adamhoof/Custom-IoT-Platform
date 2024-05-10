cf5459591eac49fa588dd7a51256788ff77e5a8b
adamhoof <hofmaad2@fel.cvut.cz>
2024-05-10 12:19:37 +0200

    Handle client disconnection properly in sse state handler - fixed problem with page reloading breaking the sse update channeling to the frontend

--- internal/http_handlers/sse_state_handler.go
+++ internal/http_handlers/sse_state_handler.go
@@ -8,38 +8,62 @@ import (
 	"net/http"
 )
 
-func SseStateHandler(w http.ResponseWriter, db *db.Database, sseChannel chan model.Update) {
-	w.Header().Set("Access-Control-Allow-Origin", "*") // or a more specific domain
+func SseStateHandler(w http.ResponseWriter, r *http.Request, db *db.Database, sseChannel chan model.Update) {
+	fmt.Println("setting up a new connection")
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	for update := range sseChannel {
-		res := fmt.Sprintf("event: stateUpdate-%d-%s\ndata: %s\n\n", update.DeviceID, update.ActionName, update.State)
-		fmt.Println(res)
-		data := map[string]interface{}{
-			"deviceID":   update.DeviceID,
-			"actionName": update.ActionName,
-			"state":      update.State,
-		}
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
 
-		stateMap := map[string]interface{}{
-			update.ActionName: update.State}
-		err := db.UpdateDeviceState(update.DeviceID, stateMap)
-		if err != nil {
-			fmt.Printf("unable to update state: %s\n", err)
-		}
+	ctx := r.Context()
 
-		jsonData, err := json.Marshal(data)
-		if err != nil {
-			fmt.Println("Failed to serialize update to JSON:", err)
-			continue // Skip this update or handle error appropriately
-		}
+	for {
+		select {
+		case update := <-sseChannel:
+			// Prepare data to send via SSE.
+			res := fmt.Sprintf("event: stateUpdate-%d-%s\ndata: %s\n\n", update.DeviceID, update.ActionName, update.State)
+			fmt.Println(res) // Logging for debugging purposes.
 
-		_, err = fmt.Fprintf(w, "data: %s\n\n", jsonData)
-		if err != nil {
-			fmt.Println("error printing state update data!")
+			// Map to update device state in the database.
+			stateMap := map[string]interface{}{
+				update.ActionName: update.State,
+			}
+
+			// Update state in the database.
+			err := db.UpdateDeviceState(update.DeviceID, stateMap)
+			if err != nil {
+				fmt.Printf("Unable to update state: %s\n", err)
+				continue // Decide if you want to skip sending the event.
+			}
+
+			// Serialize data into JSON format for sending.
+			jsonData, err := json.Marshal(map[string]interface{}{
+				"deviceID":   update.DeviceID,
+				"actionName": update.ActionName,
+				"state":      update.State,
+			})
+			if err != nil {
+				fmt.Println("Failed to serialize update to JSON:", err)
+				continue // Skip this update if serialization fails.
+			}
+
+			// Send the SSE data to the client.
+			_, err = fmt.Fprintf(w, "data: %s\n\n", jsonData)
+			if err != nil {
+				fmt.Println("Error printing state update data:", err)
+				continue
+			}
+			flusher.Flush()
+
+		case <-ctx.Done():
+			// Handle client disconnection.
+			fmt.Println("Client disconnected, closing SSE connection.")
+			return // Exit the handler when the client disconnects.
 		}
-		w.(http.Flusher).Flush()
 	}
 }
